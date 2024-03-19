e5c30c7bc72f058d591bf0d497631bad896634a6
xhermit <xhermitone@gmail.com>
2024-03-19 15:15:19 +0700

    Продолжение разработки

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+# !/bin/sh
+
+sudo apt install --assume-yes python3-pip
+
+# sudo apt install --assume-yes python3-rich
+pip3 install rich
+
+sudo apt install --assume-yes neofetch
+sudo apt install --assume-yes htop
+sudo apt install --assume-yes lnav
--- xack_tools.py
+++ xack_tools.py
@@ -9,10 +9,10 @@ import os
 try:
     import rich.console
 except ImportError:
-    print(u'Not found rich library. Install: sudo apt install python3-rich')
+    print(u'Not found rich library. Install: pip3 install rich')
     sys.exit(1)
 
-__version__ = (0, 0, 1, 1)
+__version__ = (0, 0, 1, 2)
 
 CONSOLE = rich.console.Console()
 
@@ -45,39 +45,46 @@ MAIN_MENUITEMS = '''
 '''  # assuming you want to display menulist, having it as a tuple is useless
 
 
-def main_menu():
+def main_menu(show_logo=True, show_menu=True):
     """
     Main menu function.
     :return:
     """
-    print_logo()
-    CONSOLE.print(MAIN_MENUITEMS, style='green')
+    if show_logo:
+        print_logo()
+    if show_menu:
+        CONSOLE.print(MAIN_MENUITEMS, style='green')
 
     target = CONSOLE.input('Pick an item from the menu / Выберите номер элемента меню: ')
 
     if target == '1':  # this is an equality operator, whereas = is used to assign a variable (This checks the equality basically)
-        information_menu()
+        information_menu(show_logo=False, show_menu=True)
     elif target == '0':
         CONSOLE.print('...exit', style='green')  # excessive indenting
         return
 
-    return main_menu()
+    return main_menu(show_logo=True, show_menu=True)
 
 
 INFORMATION_MENUITEMS = '''
 1. System information / Информация о системе
 2. Processes / Процессы
+3. System log / Системный журнал
+4. Kernel log / Журнал ядра
+5. Authorization log / Журнал входа пользователей в систему
 0. Return / Возврат
 '''
 
 
-def information_menu():
+def information_menu(show_logo=True, show_menu=True):
     """
     Information menu function.
     :return:
     """
-    print_logo()
-    CONSOLE.print(INFORMATION_MENUITEMS, style='green')
+    if show_logo:
+        print_logo()
+    if show_menu:
+        CONSOLE.print(INFORMATION_MENUITEMS, style='green')
 
     target = CONSOLE.input('Pick an item from the menu / Выберите номер элемента меню: ')
 
@@ -91,14 +98,22 @@ def information_menu():
         os.system('route')
         os.system('echo -n "Press any key..."')
         os.system('read ANSWER')
-        return information_menu()
     elif target == '2':
         os.system('htop')
-        return information_menu()
+    elif target == '3':
+        os.system('lnav /var/log/syslog')
+    elif target == '4':
+        os.system('lnav /var/log/kern.log')
+    elif target == '5':
+        os.system('lnav /var/log/auth.log')
     elif target == '0':
         return
+    else:
+        return information_menu(show_logo=False, show_menu=False)
 
-    return information_menu()
+    print_logo()
+    CONSOLE.print(MAIN_MENUITEMS, style='green')
+    return information_menu(show_logo=False, show_menu=True)
 
 
 def main():
